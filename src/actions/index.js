9f6f1cf185b5fd45e45d57537eb4f64d8c35f0c3
Moses Ogwo <mosesogwo@gmail.com>
2020-02-04 18:00:17 +0100

    add Proptypes dependency

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,11 @@
-const CREATE_BOOK = (book) => {
-  return ({
-    type: 'CREATE_BOOK',
-    book,
-  })
-}
+const CREATE_BOOK = book => ({
+  type: 'CREATE_BOOK',
+  book,
+});
 
-const REMOVE_BOOK = (book) => {
-  return({
-    type: 'REMOVE_BOOK',
-    id: book.id,
-  })
-}
+const REMOVE_BOOK = book => ({
+  type: 'REMOVE_BOOK',
+  id: book.id,
+});
 
-export { CREATE_BOOK, REMOVE_BOOK }
+export { CREATE_BOOK, REMOVE_BOOK };
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Book = (props) => {
-  const { book } = props
+const Book = props => {
+  const { book } = props;
   return (
     <tr>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
     </tr>
-  )
-}
+  );
+};
 
 export default Book;
+
+Book.PropTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  category: PropTypes.string,
+}
