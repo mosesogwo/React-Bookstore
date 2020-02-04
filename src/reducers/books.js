64bcda7a71fbc4163cb300a927382ec740daff97
Moses Ogwo <mosesogwo@gmail.com>
2020-02-04 16:27:22 +0100

    implement booksReducer

---
+++ src/reducers/books.js
@@ -0,0 +1,13 @@
+
+const booksReducer = (state=[], action) => {
+  switch(action.type){
+    case 'CREATE_BOOK':
+      return [...state, action.book];
+    case 'REMOVE_BOOK':
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state
+  }
+}
+
+export default booksReducer;
