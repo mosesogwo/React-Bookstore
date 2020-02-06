52d1059a8c18faed955039a662da2b0d13ae48e4
Moses Ogwo <mosesogwo@gmail.com>
2020-02-06 11:04:30 +0100

    modify Book component to return divs instead of table rows

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,14 +4,25 @@ import PropTypes from 'prop-types';
 const Book = props => {
   const { book, removeBook } = props;
 
+  // return (
+  //   <tr>
+  //     <td>{book.id}</td>
+  //     <td>{book.title}</td>
+  //     <td>{book.category}</td>
+  //     <td><button type="button" onClick={() => removeBook(book)}> Remove </button></td>
+  //   </tr>
+  // );
+
   return (
-    <tr>
-      <td>{book.id}</td>
-      <td>{book.title}</td>
-      <td>{book.category}</td>
-      <td><button type="button" onClick={() => removeBook(book)}> Remove </button></td>
-    </tr>
-  );
+    <div>
+      <div>{book.category}</div>
+      <div>
+        { book.title }
+        <byline>Author's Name</byline>
+      </div>
+    <div><button type="button" onClick={() => removeBook(book)}> Remove </button></div>
+    </div>
+  )
 };
 
 
