edb167a6b6f658d810bc9b8db38ab3d98e85aa27
Moses Ogwo <mosesogwo@gmail.com>
2020-02-06 11:51:52 +0100

    remove commented code in Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,15 +4,6 @@ import PropTypes from 'prop-types';
 const Book = props => {
   const { book, removeBook } = props;
 
-  // return (
-  //   <tr>
-  //     <td>{book.id}</td>
-  //     <td>{book.title}</td>
-  //     <td>{book.category}</td>
-  //     <td><button type="button" onClick={() => removeBook(book)}> Remove </button></td>
-  //   </tr>
-  // );
-
   return (
     <div className='book'>
       <div className='book-details'>
