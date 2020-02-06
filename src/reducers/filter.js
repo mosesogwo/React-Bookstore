7aa7104455ecffa0828956d0f3d5222e20905424
Moses Ogwo <mosesogwo@gmail.com>
2020-02-06 09:52:33 +0100

    remove All from redux store in filter.js

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,10 +1,10 @@
-const filterReducer = (state='All', action) => {
+const filterReducer = (state='', action) => {
   switch(action.type){
     case 'CHANGE_FILTER':
       console.log(action.filter)
       return action.filter;
     default:
-      return state;
+      return 'All';
   }
 }
 
