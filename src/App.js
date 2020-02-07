0e0db8aa4bfc4a73cd7ee1d699fa072b793e73a6
Moses Ogwo <mosesogwo@gmail.com>
2020-02-07 12:49:54 +0100

    remove nav from App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ function App() {
     <div>
       <header className="">
         <h1 id="home">Magic Books CMS</h1>
-        <nav>
-          <a href="#home">BOOKS</a>
-          <a href="#home">CATEGORIES</a>
-        </nav>
       </header>
       <BooksList />
       <BooksForm />
